ce7ad873eaec6f8cf3e78afa3e9401b5a27f37aa
willymariino <wg.mariino@gmail.com>
2025-04-16 15:45:09 +0200

    added homepage

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,10 @@
+function Homepage(){
+    return (
+        <>
+<h1> home</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima neque eligendi eos distinctio quos nemo, possimus, magnam eius voluptatum atque, aliquam numquam. Nihil maiores magni commodi doloribus consequuntur suscipit. Laboriosam?</p>
+</>
+    )
+}
+
+export default Homepage
