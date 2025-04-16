fa283ffd3ea71dca0d0eed6ec97bdece43cd9fb4
willymariino <wg.mariino@gmail.com>
2025-04-16 20:05:52 +0200

    added axios

--- src/pages/lista-post.jsx
+++ src/pages/lista-post.jsx
@@ -1,12 +1,43 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 import Navbar from "../components/navbar"
 
+
+
 function Posts(){
+    const [posts, setPosts] = useState([])
+
+    function fetchPosts() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => setPosts(res.data))
+        .catch((error) => console.error("errore durante il caricamento", error))
+    }
+
+    useEffect(fetchPosts, [])
+
     return(
         <>
         <h1>tutti nostri post</h1>
         <Navbar/>
 
         <p>lista post</p>
+
+        <ul>
+
+        {posts.map((post) => (
+        <li key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+
+</li>
+
+
+
+))}
+
+</ul>
+
+       
         </>
     )
 
