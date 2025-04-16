0d863525de77f97afd8b52df946165636da137e0
willymariino <wg.mariino@gmail.com>
2025-04-16 15:24:17 +0200

    added routets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
  
 
   return (
    <BrowserRouter>
-   
-   
+
+   <Routes>
+
+    <Route path="/" component={homepage} />
+    <Route path="/chi-siamo" Component={about} />
+    <Route path="/lista-post" Component={posts} />
+
+
+   </Routes>
    
    </BrowserRouter>
   )
