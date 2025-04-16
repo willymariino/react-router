945007c013f48416d8afd17b9cb8866ba69e6d12
willymariino <wg.mariino@gmail.com>
2025-04-16 19:34:32 +0200

    styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,9 @@ import {NavLink} from "react-router-dom"
 function Navbar(){
 return(
     <ul>
-        <li> <NavLink to="/homepage" >Home Page</NavLink> </li>
-        <li> <NavLink to="/about">Chi Siamo</NavLink> </li>
-        <li> <NavLink to="/lista-post">lista posts</NavLink> </li>
+        <li> <NavLink to="/homepage" className="nav-link" >Home Page</NavLink> </li>
+        <li> <NavLink to="/about" className="nav-link">Chi Siamo</NavLink> </li>
+        <li> <NavLink to="/lista-post" className="nav-link">lista posts</NavLink> </li>
     </ul>
 
 
