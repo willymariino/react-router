0cf2c013a19d912efaf08386415c50d54773bb79
willymariino <wg.mariino@gmail.com>
2025-04-17 15:29:29 +0200

    added defaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/homepage"
 import About from "./pages/about"
 import Posts from "./pages/lista-post"
+import DefaultLayout from "./layouts/defaultLayout"
 
 function App() {
- 
+
 
   return (
     <>
-   
-   <BrowserRouter>
-  
+
+      <BrowserRouter>
 
 
-   <Routes>
-    
-   <Route path="/homepage" Component={Homepage} /> 
-   <Route path="/about" Component={About} /> 
-   <Route path="/lista-post" Component={Posts} />
 
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/homepage" Component={Homepage} />
+            <Route path="/about" Component={About} />
+            <Route path="/lista-post" Component={Posts} />
+          </Route>
 
-   </Routes>
-   
-   </BrowserRouter>
-   </>
+        </Routes>
+
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/layouts/defaultLayout.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/navbar";
+import { Outlet } from "react-router-dom";
+
+function DefaultLayout() {
+    return (
+        <>
+            <Navbar />
+
+            <Outlet />
+
+
+        </>
+    )
+}
+
+export default DefaultLayout
