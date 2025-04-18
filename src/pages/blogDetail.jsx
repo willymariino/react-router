31940f8dc705721494b280d04cd5937a7a47341c
willymariino <wg.mariino@gmail.com>
2025-04-18 19:31:06 +0200

    added attributes

--- src/pages/blogDetail.jsx
+++ src/pages/blogDetail.jsx
@@ -37,6 +37,14 @@ function BlogDetail() {
             <p> type: {post.type}</p>
             <p> alignment: {post.alignment}</p>
 
+            <h2>attributes</h2>
+            <p> strenght: {post.strenght} </p>
+            <p> dexterity:{post.dexterity}</p>
+            <p> constitution: {post.constitution}</p>
+            <p>  intelligence: {post.intelligence} </p>
+            <p> wisdom: {post.wisdom} </p>
+            <p> charisma: {post.charima}</p>
+
 
         </>
     )
