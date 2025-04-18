8d9b386e5a208c09dca7adc170bb3a94bcfb16fc
willymariino <wg.mariino@gmail.com>
2025-04-18 18:22:28 +0200

    added axios to blogDetail

--- src/pages/blogDetail.jsx
+++ src/pages/blogDetail.jsx
@@ -1,8 +1,24 @@
-import { Link, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
 function BlogDetail() {
 
+    const [post, setPost] = useState(null)
     const { id } = useParams()
 
+    function getPost() {
+        axios.get("https://www.dnd5eapi.co/api/monsters/" + id)
+            .then((res) => {
+                console.log("dati ricevuti", res.data)
+                setPost(res.data)
+            })
+            .catch((error) => console.error("errore durante il caricamento", error))
+        console.log("chiamata Api")
+        console.log(id)
+
+    }
+
+    useEffect(getPost, [id]) // come fare una chiamata API al caricamento della pagina(montaggio del componente):useEffect
 
     return (
         <>
