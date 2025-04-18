97feef55a65fda898ce714b61f08a2e41c98d3c2
willymariino <wg.mariino@gmail.com>
2025-04-18 19:13:52 +0200

    added setloading

--- src/pages/blogDetail.jsx
+++ src/pages/blogDetail.jsx
@@ -4,13 +4,16 @@ import axios from "axios"
 function BlogDetail() {
 
     const [post, setPost] = useState(null)
+    const [loading, setLoading] = useState(true)
     const { id } = useParams()
 
     function getPost() {
+        setLoading(true)
         axios.get("https://www.dnd5eapi.co/api/monsters/" + id)
             .then((res) => {
-                console.log("dati ricevuti", res.data)
+                console.log("dati ricevuti", JSON.stringify(res.data, null, 2))
                 setPost(res.data)
+                setLoading(false)
             })
             .catch((error) => console.error("errore durante il caricamento", error))
         console.log("chiamata Api")
@@ -19,6 +22,12 @@ function BlogDetail() {
     }
 
     useEffect(getPost, [id]) // come fare una chiamata API al caricamento della pagina(montaggio del componente):useEffect
+
+    if (loading) {
+        return (
+            <div>caricamento in corso</div>
+        )
+    }
 
     return (
         <>
