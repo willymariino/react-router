bc027e7d1fc56caa7af2388f98dc70b7967d4a55
willymariino <wg.mariino@gmail.com>
2025-04-19 17:06:08 +0200

    styling

--- src/pages/blogDetail.jsx
+++ src/pages/blogDetail.jsx
@@ -39,29 +39,34 @@ function BlogDetail() {
                 </div>
 
                 <div className="text-container">
-                    <p>name: {post.name}</p>
-                    <p> size: {post.size}</p>
-                    <p> type: {post.type}</p>
-                    <p> alignment: {post.alignment}</p>
 
-                    <p>Armor Class</p>
-                    <ul>
-                        {post.armor_class.map((armor, index) => (
-                            <li key={index}>{armor.type}: {armor.value}</li>
-                        )
+                    <div className="general-info">
+                        <p>name: {post.name}</p>
+                        <p> size: {post.size}</p>
+                        <p> type: {post.type}</p>
+                        <p> alignment: {post.alignment}</p>
 
-                        )}
+                        <p>Armor Class</p>
+                        <ul>
+                            {post.armor_class.map((armor, index) => (
+                                <li key={index}>{armor.type}: {armor.value}</li>
+                            )
+
+                            )}
 
 
-                    </ul>
+                        </ul>
+                    </div>
 
-                    <h2>attributes</h2>
-                    <p> strenght: {post.strength} </p>
-                    <p> dexterity:{post.dexterity}</p>
-                    <p> constitution: {post.constitution}</p>
-                    <p>  intelligence: {post.intelligence} </p>
-                    <p> wisdom: {post.wisdom} </p>
-                    <p> charisma: {post.charisma}</p>
+                    <div className="attributes">
+                        <h2>attributes</h2>
+                        <p> strenght: {post.strength} </p>
+                        <p> dexterity:{post.dexterity}</p>
+                        <p> constitution: {post.constitution}</p>
+                        <p>  intelligence: {post.intelligence} </p>
+                        <p> wisdom: {post.wisdom} </p>
+                        <p> charisma: {post.charisma}</p>
+                    </div>
                 </div>
             </div>
         </>
